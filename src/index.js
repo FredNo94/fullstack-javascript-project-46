c04b9e0b030154ae70c8a3b8c07172a010e73a4a
FredNo94 <galenkov9@gmail.com>
2024-06-23 23:42:04 +0300

    Add description programm gendiff

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { program } from 'commander';
+
+const gendiff = (valueOne, valueTwo) => {
+
+};
+
+program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0');
+
+program.parse();
+
+export default gendiff;
