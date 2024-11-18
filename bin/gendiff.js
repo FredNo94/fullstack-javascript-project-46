e2cde553c1075a25b33e006aa4eb8e6baf1a0fb5
FredNo94 <galenkov9@gmail.com>
2024-11-18 10:25:32 +0300

    Update structure project

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,27 +1,5 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
-import { cwd } from 'node:process';
-import { resolve } from 'node:path';
-import parseFile from '../src/parsers.js';
-import compare from '../src/compare.js';
-import outputInFormat from '../formatters/index.js';
+import genDiff from '../src/index.js';
 
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .argument('<filepath1>', 'Введите путь до первого файла')
-  .argument('<filepath2>', 'Введите путь до второго файла')
-  .action((filepath1, filepath2, type) => {
-    const currentPath = cwd();
-    const fileOne = resolve(currentPath, 'utils', filepath1);
-    const fileTwo = resolve(currentPath, 'utils', filepath2);
-    const objOne = parseFile(fileOne);
-    const objTwo = parseFile(fileTwo);
-    const resultCompare = compare(objOne, objTwo);
-    console.log(outputInFormat(type, resultCompare));
-  });
-
-program.parse();
+genDiff();
